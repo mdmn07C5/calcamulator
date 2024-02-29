7512320a1b3136f9e05e11ef9afbd10e8ac8044c
mdmn07C5 <malicdan@protonmail.ch>
2024-02-29 16:02:52 -0500

    Add keys to the keypad
    builds the actual keypad to show on the webpage
    keys are modelled after ti-30x2s lmao
    also filled in the functions for the 4 basic ops

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 function add(a, b) {
-
+    return a + b;
 }
 
 function subtract(a, b) {
-
+    return a - b;
 }
 
 function multiply(a, b) {
-
+    return a * b;
 }
 
 function divide(a, b) {
-
+    return a / b;
 }
+
+function buildKey(val) {
+    const key = document.createElement('button');
+    key.setAttribute('id', `key-${val}`);
+    key.setAttribute('class', 'key-button');
+    key.textContent = val;
+    return key;
+}
+
+function buildKeypad() {
+    const KEYS = [
+        ['7', '8', '9', '/'],
+        ['4', '5', '6', '*'],
+        ['1', '2', '3', '-'],
+        ['0', '.', '(-)', '=']
+    ]
+
+    const keypad = document.querySelector('#keypad');
+    KEYS.forEach((row) => {
+        const keypadRow = document.createElement('div');
+        keypadRow.setAttribute('class', 'keypad-row');
+        row.forEach((val) => {
+            const key = buildKey(val);
+            keypadRow.appendChild(key);
+        })
+
+        keypad.appendChild(keypadRow);
+    })
+}
+
+buildKeypad();
